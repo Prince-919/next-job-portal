9ab041ce029f9f651b61fda2a73a91820e360516
Prince-919 <princesharmabbu0@gmail.com>
2024-09-21 23:13:52 +0530

    add user model

---
+++ src/models/userModel.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// delete old model
+const UserModel = mongoose.model("User");
+mongoose.deleteModel(UserModel.modelName);
+
+// craete new model
+module.exports = mongoose.model("User", userSchema);
