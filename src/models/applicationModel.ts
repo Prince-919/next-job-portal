c59663d92177ee34b8047f2e8871a0af5e536748
Prince-919 <princesharmabbu0@gmail.com>
2024-09-24 12:52:55 +0530

    add applications model

---
+++ src/models/applicationModel.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const applicationSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+    },
+    job: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "jobs",
+    },
+    status: {
+      type: String,
+      enum: ["pending", "accepted", "rejected"],
+    },
+  },
+  { timestamps: true }
+);
+
+// delete old model
+if (mongoose.models.applications) {
+  const ApplicationModel = mongoose.model("applications");
+  mongoose.deleteModel(ApplicationModel.modelName);
+}
+// craete new model
+const Application = mongoose.model("applications", applicationSchema);
+export default Application;
