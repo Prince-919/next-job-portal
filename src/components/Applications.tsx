5ba15eb111370ffa56c650461dfbdd8a88614799
Prince-919 <princesharmabbu0@gmail.com>
2024-09-24 16:40:17 +0530

    small change application model get application

--- src/components/Applications.tsx
+++ src/components/Applications.tsx
@@ -1,6 +1,11 @@
 "use client";
 
-import { Modal } from "antd";
+import { setLoading } from "@/redux/loaderSlice";
+import { message, Modal, Table } from "antd";
+import axios from "axios";
+import moment from "moment";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function Applications({
   selectedJob,
@@ -11,11 +16,70 @@ export default function Applications({
   showApplications: boolean;
   setShowApplications: boolean;
 }) {
+  const [applications, setApplications] = useState([]);
+  const dispatch = useDispatch();
+
+  async function fetchApplications() {
+    try {
+      dispatch(setLoading(true));
+      const response = await axios.get(
+        `/api/applications?job=${selectedJob._id}`
+      );
+      setApplications(response.data.data);
+    } catch (error: any) {
+      message.error(error.message);
+    } finally {
+      dispatch(setLoading(false));
+    }
+  }
+
+  useEffect(() => {
+    fetchApplications();
+  }, []);
+
+  const columns = [
+    {
+      title: "Application Id",
+      dataIndex: "_id",
+    },
+    {
+      title: "Applicant",
+      dataIndex: "user",
+      render: (user: any) => user.name,
+    },
+    {
+      title: "Email",
+      dataIndex: "user",
+      render: (user: any) => user.email,
+    },
+    {
+      title: "Applied On",
+      dataIndex: "createdAt",
+      render: (text: any) => moment(text).format("DD-MM-YYYY"),
+    },
+    {
+      title: "Status",
+      dataIndex: "status",
+      render: (status: string) => (
+        <select value={status}>
+          <option value="pending">Pending</option>
+          <option value="shortlisted">Shortlisted</option>
+          <option value="rejected">Rejected</option>
+        </select>
+      ),
+    },
+  ];
   return (
     <Modal
-      title="Applications"
+      title={`Applications for ${selectedJob.title}`}
       open={showApplications}
       onCancel={() => setShowApplications(false)}
-    ></Modal>
+      width={1000}
+      closeIcon={false}
+    >
+      <div className="my-3">
+        <Table columns={columns} dataSource={applications} />
+      </div>
+    </Modal>
   );
 }
